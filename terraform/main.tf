ebe55ec1b714e74bf0a14c0b2195f04e05b6e0e3
tereza-arta <157483329+tereza-arta@users.noreply.github.com>
2024-12-14 23:39:54 +0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,86 +1,26 @@
-resource "azurerm_resource_group" "some" {
-  name     = "rg-example"
-  location = "North Europe"
+module "network" {
+  source = "./modules/network_components"
+
+  enable_net = var.enable_net
+  count      = var.enable_net ? 1 : 0
 }
 
-data "azurerm_platform_image" "platform_img" {
-  location  = azurerm_resource_group.some.location
-  publisher = "Canonical"
-  offer     = "0001-com-ubuntu-server-focal"
-  sku       = "20_04-lts"
+module "nsg" {
+  source = "./modules/security_group"
+
+  enable_nsg        = var.enable_nsg
+  count             = var.enable_nsg ? 1 : 0
+  location          = module.network[0].location
+  rg_name           = module.network[0].rg_name
+  net_interface_ids = module.network[0].net_interface_ids
 }
 
-resource "azurerm_virtual_network" "net" {
-  name                = "net-example"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.some.location
-  resource_group_name = azurerm_resource_group.some.name
-}
+module "vm" {
+  source = "./modules/vm_config"
 
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet-example"
-  resource_group_name  = azurerm_resource_group.some.name
-  virtual_network_name = azurerm_virtual_network.net.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_public_ip" "ip" {
-  count = var.pub_ip_cnt
-  name  = "pub-ip-${count.index + 1}"
-  #name = "mpub-ip-1"
-  location            = azurerm_resource_group.some.location
-  resource_group_name = azurerm_resource_group.some.name
-  allocation_method   = "Static"
-  sku                 = "Basic"
-}
-
-resource "azurerm_network_interface" "nic" {
-  count = var.nic_cnt
-  name  = "nic-${count.index + 1}"
-  #name = "mnic-1"
-  location            = azurerm_resource_group.some.location
-  resource_group_name = azurerm_resource_group.some.name
-
-  ip_configuration {
-    name                          = "ip-config"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.ip[count.index].id
-    #public_ip_address_id = azurerm_public_ip.ip.id
-  }
-}
-
-resource "azurerm_network_interface_security_group_association" "nsg-nic" {
-  count                     = var.ass_cnt
-  network_interface_id      = azurerm_network_interface.nic[count.index].id
-  network_security_group_id = azurerm_network_security_group.nsg.id
-}
-
-resource "azurerm_linux_virtual_machine" "vm" {
-  count = var.vm_cnt
-  name  = format("%s-%d", var.vm_name, count.index + 1)
-  #name = "azure-vm-1"
-  resource_group_name   = azurerm_resource_group.some.name
-  location              = azurerm_resource_group.some.location
-  size                  = var.vm_size
-  admin_username        = "azureuser"
-  network_interface_ids = [azurerm_network_interface.nic[count.index].id]
-  #network_interface_ids = [azurerm_network_interface.nic.id]
-
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = data.azurerm_platform_image.platform_img.publisher
-    offer     = data.azurerm_platform_image.platform_img.offer
-    sku       = data.azurerm_platform_image.platform_img.sku
-    version   = data.azurerm_platform_image.platform_img.version
-  }
+  enable_vm         = var.enable_vm
+  count             = var.enable_vm ? 1 : 0
+  location          = module.network[0].location
+  rg_name           = module.network[0].rg_name
+  net_interface_ids = module.network[0].net_interface_ids
 }
