14acfa13ab5580e2c205c7ff4687f5f9acedbb6a
tereza-arta <157483329+tereza-arta@users.noreply.github.com>
2024-12-15 20:37:46 +0400

    Add comments

--- ansible/roles/access_to_azure/files/preinstall.sh
+++ ansible/roles/access_to_azure/files/preinstall.sh
@@ -1,26 +1,23 @@
 exec > /tmp/preinstall.log 2>&1
 set -x
 
+//Install Ansible
 sudo apt update
 sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
 
+//Install specific collection with requirements
 ansible-galaxy collection install azure.azcollection --force
-#or apt-get
 sudo apt install python3-pip -y
-
 sudo pip3 install -r ~/.ansible/collections/ansible_collections/azure/azcollection/requirements.txt
 
+//Preconfigure Azure installation
 sudo apt update
-
 sudo apt install apt-transport-https ca-certificates curl gnupg lsb-release -y
-
 sudo mkdir -p /etc/apt/keyrings
-
 curl -sLS https://packages.microsoft.com/keys/microsoft.asc |
   gpg --dearmor | sudo tee /etc/apt/keyrings/microsoft.gpg > /dev/null
-
 sudo chmod go+r /etc/apt/keyrings/microsoft.gpg
 
 AZ_DIST=$(lsb_release -cs)
@@ -33,5 +30,4 @@ Architectures: $(dpkg --print-architecture)
 Signed-by: /etc/apt/keyrings/microsoft.gpg" | sudo tee /etc/apt/sources.list.d/azure-cli.sources
 
 sudo apt update
-
 sudo apt install azure-cli
