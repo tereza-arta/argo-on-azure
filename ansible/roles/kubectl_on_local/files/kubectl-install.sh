594e00c50647eacf312c5806dd81275f6a8c1548
Tereza <you@example.com>
2024-12-26 14:02:52 +0400

    Add two roles for local kubectl access

---
+++ ansible/roles/kubectl_on_local/files/kubectl-install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+exec > /tmp/kubectl-install-debug.log 2>&1
+set -x
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+//Check kubectl installation
+kubectl version --client
+
